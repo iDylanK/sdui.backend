d144e610b8cfe231b0ae926d3ebb40c1c7c891f6
iDylanK <dylankieft@live.nl>
2022-06-23 10:06:36 +0200

    Added helper screens to validation app

--- validation_app/src/controllers/validation.ts
+++ validation_app/src/controllers/validation.ts
@@ -7,12 +7,29 @@ import {
 
 const getProductsScreen = (req: Request, res: Response, next: NextFunction) => res.status(200).json(
     screen({
-    })
+        id: 'screen-products',
+        type: 'LIST',
+        content: {
+            sections: [
+                {
+                    components: [
+                        {
+                            id: 'component-0',
+                            type: 'BASIC',
+                            content: 'Hello world',
+                        },
+                    ],
+                },
+            ],
+        },
+    }),
 );
 
 const getProductScreen = (req: Request, res: Response, next: NextFunction) => res.status(200).json(
     screen({
-    })
+        id: 'screen-product',
+        type: 'CONTENT_VIEW',
+    }),
 );
 
 // const getHome = (req: Request, res: Response, next: NextFunction) => getLibrary(req, res, next);
